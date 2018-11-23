087153b5b7ca4461dff1a908b476bfcbbecd4646
David Lebee <david@poweredsoft.com>
2018-11-23 01:42:53 -0600

    better

---
+++ PoweredSoft.DynamicQuery/ServiceCollectionExtensions.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using PoweredSoft.DynamicQuery.Core;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PoweredSoft.DynamicQuery
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddPoweredSoftDynamicQuery(this IServiceCollection services)
+        {
+            services.TryAddTransient<ISort, Sort>();
+            services.TryAddTransient<IAggregate, Aggregate>();
+            services.TryAddTransient<ISimpleFilter, SimpleFilter>();
+            services.TryAddTransient<ICompositeFilter, CompositeFilter>();
+            services.TryAddTransient<IGroup, Group>();
+            services.TryAddTransient<IQueryCriteria, QueryCriteria>();
+            services.TryAddTransient<IQueryHandler, QueryHandler>();
+            return services;
+        }
+    }
+}
