3a11db6dd7f2d12748f602a82dd1ff5b704acf23
David Lebee <david@poweredsoft.com>
2019-04-22 22:07:56 -0500

    fixing issue with composite filter serialization

---
+++ PoweredSoft.DynamicQuery.Test/DeserializeTests.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using PoweredSoft.DynamicQuery.AspNetCore.Json;
+using PoweredSoft.DynamicQuery.Core;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace PoweredSoft.DynamicQuery.Test
+{
+    public class SerializationTests
+    {
+        [Fact]
+        public void QueryCriteria()
+        {
+            var json = @"{""page"":1,""pageSize"":20,""filters"":[{""type"":""composite"",""filters"":[{""path"":""title"",""value"":""Qui"",""type"":""StartsWith"",""and"":false}]}]}";
+
+            var serviceCollection = new ServiceCollection();
+            serviceCollection.AddPoweredSoftDynamicQuery();
+            var serviceProvider = serviceCollection.BuildServiceProvider();
+
+            var settings = new JsonSerializerSettings();
+            settings.Converters.Add(new StringEnumConverter());
+            settings.Converters.Add(new DynamicQueryJsonConverter(serviceProvider));
+       
+            var data = JsonConvert.DeserializeObject<IQueryCriteria>(json, settings);
+            Assert.NotNull(data);
+        }
+
+        
+    }
+}
