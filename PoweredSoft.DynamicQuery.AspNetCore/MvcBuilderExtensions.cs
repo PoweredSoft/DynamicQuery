958122a0a0f5b41933aa477d774fc4fc6b66c068
pampas <mali@ku-soft.com>
2020-04-08 18:05:01 +0800

    合并扩展方法项目

--- PoweredSoft.DynamicQuery.AspNetCore/MvcBuilderExtensions.cs
+++ PoweredSoft.DynamicQuery.AspNetCore/MvcBuilderExtensions.cs
@@ -3,6 +3,8 @@ using PoweredSoft.Data;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Text.Json.Serialization;
+using PoweredSoft.DynamicQuery.System.Text.Json;
 
 namespace PoweredSoft.DynamicQuery.AspNetCore
 {
@@ -13,6 +15,22 @@ namespace PoweredSoft.DynamicQuery.AspNetCore
             builder.Services.AddPoweredSoftDataServices();
             builder.Services.AddPoweredSoftDynamicQuery();
             return builder;
+        }
+        
+        public static IMvcBuilder AddPoweredSoftJsonNetDynamicQuery(this IMvcBuilder mvcBuilder,
+            bool enableStringEnumConverter = true)
+        {
+            mvcBuilder.AddPoweredSoftDynamicQuery();
+            var serviceProvider = mvcBuilder.Services.BuildServiceProvider();
+            mvcBuilder.AddJsonOptions(cfg =>
+            {
+                if (enableStringEnumConverter)
+                    cfg.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+                cfg.JsonSerializerOptions.Converters.Add(new DynamicQuerySimpleFilterConverter(serviceProvider));
+                cfg.JsonSerializerOptions.Converters.Add(new DynamicQuerySortConverter(serviceProvider));
+                cfg.JsonSerializerOptions.Converters.Add(new DynamicQueryJsonConverter(serviceProvider));
+            });
+            return mvcBuilder;
         }
     }
 }
